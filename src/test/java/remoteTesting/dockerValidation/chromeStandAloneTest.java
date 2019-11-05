70857d57bf9a8800e8678f527a598eab34ef7127
Greg <gregsmith446@gmail.com>
2019-11-04 19:43:04 -0500

    successful run with Docker and Chrome

---
+++ src/test/java/remoteTesting/dockerValidation/chromeStandAloneTest.java
@@ -0,0 +1,26 @@
+package remoteTesting.dockerValidation;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class chromeStandAloneTest {
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+		// SETUP REMOTE WEB DRIVER
+		
+		// java class "URL"
+		URL u = new URL("http://localhost:4444/wd/hub");
+		// selenium class "capabilities"
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		// selenium class "RemoteWebDriver" takes as parameters (url, capabilities)
+		RemoteWebDriver driver = new RemoteWebDriver(u, cap);
+		
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
+	}
+
+}
