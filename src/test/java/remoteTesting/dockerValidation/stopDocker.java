c1cfd78a943d44d47959b3a87db96affcc6a844c
Greg <gregsmith446@gmail.com>
2019-11-11 18:40:13 -0500

    able to start docker, scale it to 5 chromes, shut down docker, and most times delete the log output file - last part need fine tuning on sleep time

---
+++ src/test/java/remoteTesting/dockerValidation/stopDocker.java
@@ -0,0 +1,64 @@
+package remoteTesting.dockerValidation;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Calendar;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class stopDocker {
+
+	@Test
+	public void startFile() throws IOException, InterruptedException
+	{		
+		Runtime runtime = Runtime.getRuntime();
+		runtime.exec("cmd /c start dockerDown.bat");
+		
+		String f = "output.txt";
+		boolean flag = false;
+		
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.SECOND, 70);
+		Long stopNow = cal.getTimeInMillis();
+		Thread.sleep(3000);
+		
+		while (System.currentTimeMillis() < stopNow)
+		{
+			if (flag)
+			{
+				break;
+			}
+			
+			BufferedReader reader = new BufferedReader(new FileReader(f));
+			String currentLine = reader.readLine();
+			
+			while (currentLine != null && !flag)
+			{
+				if (currentLine.contains("Shutdown complete"))
+				{
+					System.out.println("Found the server shutdown text");
+					flag = true;
+					break;
+				}
+				currentLine = reader.readLine();
+			}
+			reader.close();
+		}
+		
+		Thread.sleep(25000);
+		Assert.assertTrue(flag);
+		
+		File fi = new File("output.txt");
+		if(fi.delete())
+		{
+			System.out.println("log file deleted successfully");
+		}
+	}
+}
+
+
+
+
